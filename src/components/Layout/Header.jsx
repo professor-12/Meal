5b477f6586172b7c232aa3615383a9de5fdd75d8
Badejo Emmanuel <badejoemmanuel805@gmail.com>
2023-10-01 01:03:56 -0500

    Modified Logic , added animation and Dark mode feature

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -16,7 +16,7 @@ const Header = props => {
         <nav className='mx-auto p-6 bg-red-800 container flex md:w-[80%] justify-between'>
           <h1 className=' text-white font-semibold text-3xl'>AfroMeals</h1>
           <HeaderCartButton onClick={props.onShowcart}></HeaderCartButton>
-          <motion.div whileTap={{scale:1.4}} onClick={()=> {toggle(mode)}}  className='absolute right-5 rounded-xl p-2 cursor-wait text-white font-medium  text-sm  bg-black'> { mode }</motion.div>
+          <motion.div whileTap={{scale:1.4}} onClick={()=> {toggle(mode)}}  className='absolute right-0 rounded-xl p-2 cursor-wait text-white font-medium  text-sm  bg-black'> { mode }</motion.div>
         </nav>
         <div>
           <img src={meals} className='w-full object-cover h-[14rem]' alt='' />
